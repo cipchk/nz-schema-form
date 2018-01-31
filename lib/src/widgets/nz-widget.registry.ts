bd951cc5d8cad549fbba98811bc5c54f963d4d94
vellengs <vellengs@qq.com>
2018-01-31 23:45:12 +0800

    Update nz-widget.registry.ts

--- lib/src/widgets/nz-widget.registry.ts
+++ lib/src/widgets/nz-widget.registry.ts
@@ -5,6 +5,7 @@ import { ArrayWidget } from './array/array.widget';
 import { ButtonWidget } from './button/button.widget';
 import { StringWidget } from './string/string.widget';
 import { DateWidget } from './date/date.widget';
+import { TimeWidget } from './time/time.widget';
 import { TextareaWidget } from './textarea/textarea.widget';
 import { BooleanWidget } from './boolean/boolean.widget';
 import { NumberWidget } from './number/number.widget';
@@ -25,6 +26,7 @@ export class NzWidgetRegistry extends WidgetRegistry {
 
         this.register('string', StringWidget);
         this.register('date', DateWidget);
+        this.register('time', TimeWidget);
         this.register('boolean', BooleanWidget);
         this.register('number', NumberWidget);
         this.register('checkbox', CheckboxWidget);
