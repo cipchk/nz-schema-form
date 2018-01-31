806c66b0856cb46735684b4714c7e84791927484
Canaan <595364430@qq.com>
2018-01-31 17:36:18 +0800

    完善作者提的瑕疵

--- lib/src/widgets/radio/radio.widget.ts
+++ lib/src/widgets/radio/radio.widget.ts
@@ -5,18 +5,20 @@ import { ControlWidget } from '../../widget';
     selector: 'nz-sf-radio-widget',
     template: `
     <div nz-form-control nz-col [nzSpan]="schema.span_control" [nzOffset]="schema.offset_control">
-    <nz-radio-group [formControl]="control" >
-        <label *ngFor="let option of selectList" nz-radio [nzValue]="option.value">
-            <span [innerText]="option.label"></span>
-        </label>
-    </nz-radio-group>
-    <div nz-form-extra *ngIf="extra" [innerHTML]="extra"></div>
-    <div nz-form-explain *ngIf="!onlyVisual && hasError">{{errorMessage}}</div>
-</div>
-    `
+        <nz-radio-group [formControl]="control" >
+            <label *ngFor="let option of selectList" 
+                nz-radio 
+                [nzValue]="option.value"
+                [nzDisabled]="schema.disabled">
+                <span [innerHTML]="option.label"></span>
+            </label>
+        </nz-radio-group>
+        <div nz-form-extra *ngIf="extra" [innerHTML]="extra"></div>
+        <div nz-form-explain *ngIf="!onlyVisual && hasError">{{errorMessage}}</div>
+    </div>`
 })
 export class RadioWidget extends ControlWidget {
     get selectList() {
-        return this.widgetData['selectList'] || [];
+        return this.widgetData.selectList || [];
     }
 }
